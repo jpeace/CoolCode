d95be7e6051fad1eac949ce78dabe7b455ca7a46
Jarrod Peace <peace.jarrod@gmail.com>
2011-02-03 19:43:10 -0600

    Merged in redirectable implementation for fubu

--- Code/Source/CoolCode.Web/Endpoints/Account/Create/CreateEndpoint.cs
+++ Code/Source/CoolCode.Web/Endpoints/Account/Create/CreateEndpoint.cs
@@ -1,3 +1,6 @@
+using CoolCode.Web.Endpoints.Home.Index;
+using FubuMVC.Core.Continuations;
+
 namespace CoolCode.Web.Endpoints.Account.Create
 {
     public class CreateEndpoint
@@ -7,9 +10,14 @@ namespace CoolCode.Web.Endpoints.Account.Create
             return new CreateViewModel {Username = "", Password = "", ConfirmPassword = ""};
         }
 
-        public CreateViewModel Post(CreateRequestModel request)
+        public Redirectable<CreateViewModel> Post(CreateRequestModel request)
         {
-            return new CreateViewModel {Username = request.Username, Password = "", ConfirmPassword = "", Message = "OK"};
+            var model = new CreateViewModel {Username = request.Username, Password = "", ConfirmPassword = "", Message = "OK"};
+            if (request.Username == "jarrod")
+            {
+                return new Redirectable<CreateViewModel>(FubuContinuation.RedirectTo<IndexEndpoint>(e => e.Get()));
+            }
+            return new Redirectable<CreateViewModel>(model);
         }
     }
 }
